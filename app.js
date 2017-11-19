639a31be627fc25f0acf6150d3f97d26f886a0ed
Kriti Agarwal <kriti.agarwal@student.fh-kiel.de>
2017-11-19 14:18:21 +0100

    student dashboard updated

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Module dependencies
 var express    = require('express'),
     mysql      = require('mysql'),
-	bodyParser = require('body-parser');
+    bodyParser = require('body-parser');
 // Configuration
 // Application initialization
 var app = express();
@@ -10,6 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+var http = require('http').Server(app);
 // set the view engine to ejs -- momal
 app.set('view engine', 'ejs');
 //Set up data connection
@@ -24,9 +25,22 @@ app.use(express.static(__dirname + '/public'));
 /*----------------- No need to make any changes to this part unless any dependency is needed to be added -----------*/
 
 // ------------------SQL Queries----------------------
-//----------- Please add all sql strings here --------
+//----------- Please add all sql strings here 
 
+app.get('/', function(req, res) {
+    connection.query('select * from classes inner join user_class on user_class.classId = classes.classID inner join users on users.userID = user_class.userId and users.userID = 1;', req.body,
+        function (err, result) {
 
+            connection.query('select classes.classID, class_assessment.caID, assessment.totalMarks, assessment.passingMarks from classes inner join class_assessment on class_assessment.classId = classes.classID inner join assessment on assessment.assessmentID = class_assessment.assessmentID;',
+                function (err, classAssessment) {
+                    if (err) throw err;
+                    var classAssessmentJson =JSON.stringify(classAssessment);
+                    var jsonData = classAssessmentJson.replace(/\"([^(\")"]+)\":/g,"$1:");
+                    res.render('student_dashboard', {classesInfo: result, classassesmentInfo: jsonData});
+                });
+
+        });
+});
 
 
 
@@ -42,9 +56,11 @@ app.use(express.static(__dirname + '/public'));
 
 
 // Main route sends our HTML file
+
 app.get('/', function(req, res) {
     res.sendfile(__dirname + '/views/index.html');
 });
+
 
 
 //----------------------------------------------------
@@ -55,3 +71,4 @@ app.get('/', function(req, res) {
 
 // Begin listening
 app.listen(3000);
+console.log("server started")
