27807f6c7197565e035eb62a78e931fdcac903fe
Moomal <moomal.mahar@student.fh-kiel.de>
2017-11-27 19:21:16 +0100

    added chat functionality

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var express    = require('express'),
     path       = require('path');
 var router = express.Router();
 
+
+
 // Application initialization
 var app = express();
 app.use(bodyParser());
@@ -13,6 +15,9 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 var http = require('http').Server(app);
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
 // set the view engine to ejs -- momal
 app.set('view engine', 'ejs');
 //Set up data connection
@@ -22,6 +27,10 @@ var connection = mysql.createConnection({
     password : 'root',
     database : 'ASSESS_EASY'
 });
+
+server.listen(process.env.PORT || 3000);
+console.log("Server is running ... ");
+
 //routing the static files. css/js
 app.use(express.static(__dirname + '/public'));
 
@@ -60,6 +69,78 @@ app.use('/teacher',teacher);
 var teachers = require('./routes/teachers.js');
 var index = require('./routes/index.js');
 
+//-----------------------------------------------------------------------------------
+/*
+* This is the chat code
+* It is the first draft
+* This code would be removed from app.js
+* and added in its own file once because right now it's being a little b*tch when i add it in www
+* */
+app.get('/chat', function (req, res) {
+    res.render('chat');
+});
+users = [];
+connections = [];
+io.sockets.on('connection',function(socket) {
+    connections.push(socket);
+    console.log('Connected: %s sockets connected', connections.length);
+
+
+    //Disconnect
+    socket.on('disconnect', function (data) {
+
+        users.splice(users.indexOf(socket.username),1);
+        updateUsernames();
+        connections.splice(connections.indexOf(socket, 1));
+        console.log('Disconnected: %s sockets connected', connections.length)
+    });
+
+    //Send Message
+    socket.on('send message', function (data) {
+        console.log(data);
+        io.sockets.emit('new message',{msg: data, user: socket.username});
+    });
+
+
+    //New User
+    socket.on('new user', function (data, callback) {
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUsernames();
+    });
+
+    function updateUsernames() {
+        io.sockets.emit('get users', users);
+    }
+
+
+});
+
+// ---------------- CHAT ENDS HERE --------------------------------
+
+
+
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+
+
+
 // if there are any pages that start after localhost:8080/ then route them to index
 // this includes the main page and/or about page, contact us page etc ...
 app.use('/',index);
@@ -71,6 +152,7 @@ app.use('/teacher',teachers);
 // Begin listening
 
 
+/*
 
 app.listen(3000);
-console.log("server started")
+console.log("server started")*/
