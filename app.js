069fb7b24ec9a24d1cbf77aca9eb186a1e7a16b3
Kriti Agarwal <kriti.agarwal@student.fh-kiel.de>
2017-12-02 18:37:57 +0100

    routes added

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(express.static(__dirname + '/public'));
 /*----------------- No need to make any changes to this part unless any dependency is needed to be added -----------*/
 
 //routing to student dashboard
-var studentDashboard = require('./public/js/student_dashboard.js');
+var studentDashboard = require('./routes/student_dashboard.js');
 app.use('/',studentDashboard);
 /*--------------------------------------------------*/
 
