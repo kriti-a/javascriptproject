2827467c33f6fd7d67f8dd7a2d3475a7d01e7b46
Kriti Agarwal <kriti.agarwal@student.fh-kiel.de>
2017-12-02 12:13:02 +0100

    app.js changed

--- app.js
+++ app.js
@@ -45,20 +45,7 @@ app.use('/',studentDashboard);
 // ------------------SQL Queries----------------------
 //----------- Please add all sql strings here
 
-app.get('/', function(req, res) {
-    connection.query('select * from classes inner join user_class on user_class.classId = classes.classID inner join users on users.userID = user_class.userId and users.userID = 1;', req.body,
-        function (err, result) {
 
-            connection.query('select classes.classID, class_assessment.caID, assessment.totalMarks, assessment.passingMarks from classes inner join class_assessment on class_assessment.classId = classes.classID inner join assessment on assessment.assessmentID = class_assessment.assessmentID;',
-                function (err, classAssessment) {
-                    if (err) throw err;
-                    var classAssessmentJson =JSON.stringify(classAssessment);
-                    var jsonData = classAssessmentJson.replace(/\"([^(\")"]+)\":/g,"$1:");
-                    res.render('student_dashboard', {classesInfo: result, classassesmentInfo: jsonData});
-                });
-
-        });
-});
 
 
 //----------------------------------------------------
