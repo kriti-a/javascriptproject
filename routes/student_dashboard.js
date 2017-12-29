409eb851cb3a49431cef0711780f262a3b41cdc3
kriti-a <kriti.agarwal101090@gmail.com>
2017-12-29 11:18:02 +0100

    notification panel added

--- routes/student_dashboard.js
+++ routes/student_dashboard.js
@@ -1,6 +1,10 @@
 var express    = require('express'),
     mysql      = require('mysql'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    moment = require('moment');
+/*exports.index = function(req, res) {
+    res.render('index', { moment: moment });
+}*/
 var router = express.Router();
 //Set up data connection
 var connection = mysql.createConnection({
@@ -11,15 +15,18 @@ var connection = mysql.createConnection({
 });
 // ------------------SQL Queries----------------------
 
-router.get('/', function(req, res) {
+router.get('/student_dashboard', function(req, res) {
     connection.query('select * from classes inner join user_class on user_class.classId = classes.classID inner join users on users.userID = user_class.userId and users.userID = 1;', req.body,
         function (err, result) {
             connection.query('select classes.classID, assessment.assessmentID, assessment.passingMarks, assessment_results.obtainedMarks, users.userID from classes inner join class_assessment on class_assessment.classId = classes.classID inner join assessment on assessment.assessmentID = class_assessment.assessmentID inner join assessment_results on assessment.assessmentID = assessment_results.assessmentID inner join users on users.userID = assessment_results.userID;',
                 function (err, classAssessment) {
-                    if (err) throw err;
-                    var classAssessmentJson =JSON.stringify(classAssessment);
-                    var jsonData = classAssessmentJson.replace(/\"([^(\")"]+)\":/g,"$1:");
-                    res.render('student_dashboard', {classesInfo: result, classassesmentInfo: jsonData});
+                    connection.query('select * from notification;',
+                        function (err, notification) {
+                            if (err) throw err;
+                            var classAssessmentJson = JSON.stringify(classAssessment);
+                            var jsonData = classAssessmentJson.replace(/\"([^(\")"]+)\":/g, "$1:");
+                            res.render('student_dashboard', {classesInfo: result, classassesmentInfo: jsonData, notificationInfo: notification, moment: moment});
+                        });
                 });
 
         });
